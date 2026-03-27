c32f1213bced320601892066580caa4ee73634ef
Ajay28022003 <ajaykannapiran28@gmail.com>
2026-03-28 00:36:29 +0530

    feature completed in landing page

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,10 +1,10 @@
 export function FeatureCard({feature}) {
     return (
         <>
-        <div className="p-6 border border-slate-300">
-            <div>{feature.icon}</div>
-            <div>{feature.title}</div>
-            <div>{feature.description}</div>
+        <div className="p-6 border border-slate-800 rounded-xl hover:border-slate-500 cursor-pointer hover:bg-slate-950">
+            <div className=" border border-slate-700 w-12 h-12 rounded-lg flex items-center justify-center text-2xl mb-5">{feature.icon}</div>
+            <div className="mb-2 text-lg font-semibold text-slate-100">{feature.title}</div>
+            <div className="text-slate-600">{feature.description}</div>
         </div>
         </>
     )
--- src/pages/LandingPage/sections/Features.jsx
+++ src/pages/LandingPage/sections/Features.jsx
@@ -6,12 +6,12 @@ export default function Features() {
     return (
         <>
             <section className="flex flex-col items-center gap-3  p-4 max-w-7xl mx-auto mb-32">
-                <div className="bg-slate-800 px-3 py-1 rounded-xl"> Features</div>
+                <div className="bg-slate-800 px-3 py-1 rounded-xl mb-2"> Features</div>
                 <h2 className="text-5xl font-bold">
-                    Everything you need to learn faster
+                    Everything you need to <span className="bg-gradient-to-r from-purple-400 to-purple-600 bg-clip-text text-transparent">learn faster</span>
                 </h2>
-                <p className="flex justify-center">
-                    Powerful AI tools combined with a beautiful knowledge <br/>management system.
+                <p className="flex justify-center mb-10 text-slate-600 max-w-2xl">
+                    Powerful AI tools combined with a beautiful knowledge management system.
                 </p>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
                     {
